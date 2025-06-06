ae7064cefd9613a1e40fa1524b6310715554a746
ypkim <ypkim0923@gmail.com>
2025-06-06 16:23:00 +0900

    feat(app): Modulithic 프로젝트로 변경

--- src/main/java/com/ypkim/oauth2/Oauth2Application.java
+++ src/main/java/com/ypkim/oauth2/Oauth2Application.java
@@ -2,8 +2,9 @@ package com.ypkim.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
 
-@SpringBootApplication
+@Modulith
 public class Oauth2Application {
 
 	public static void main(String[] args) {
